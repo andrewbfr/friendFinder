1bc75a0b9e85921cee28e39fcf885226165de2e4
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-21 21:32:38 -0700

    hello world complete

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
+
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var friends = require("./app/data/friends.js");
+
+console.log("I'm a toaster");
