8fda46602d9beb8552f137f0eb7a0c6f6ab22b35
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-23 16:36:57 -0700

    routes established, api reference in order, working on survey page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,10 +7,10 @@ var path = require("path");
 module.exports = function(app) {
     //directly to survey (will be called with a button-click)
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     //default all other requests not listed to the home page
     app.get("*", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
