f603e0091ba5ac52c520ccade059fb02643c51d5
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-22 20:57:46 -0700

    periodic updates, establishing routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// * A GET Route to `/survey` which should display the survey page.
+// * A default, catch-all route that leads to `home.html` which displays the home page. 
+
+// We need to include the path package to get the correct file path for our html
+var path = require("path");
+
+module.exports = function(app) {
+    //directly to survey (will be called with a button-click)
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(_dirname, "../public/survey.html"));
+    });
+    //default all other requests not listed to the home page
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(_dirname, "../public/home.html"));
+    });
+}
