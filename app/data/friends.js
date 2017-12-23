7f192b9ba70add908614b569c7b3e5ed1a0ff1fd
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-22 21:14:51 -0700

    routing, home page complete, adding compatibility and debugging tomorrow

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,39 @@
-var friends = {};
+// receive data from submission form, store as an object
+//begin friends with some "dummy" data
+var friends = [
+    {
+        name: "Toast",
+        photo:"https://images.theconversation.com/files/92297/original/image-20150818-12436-j4pzri.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1012&h=668&fit=crop",
+        scores:[
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+
+        ]
+    },
+    {
+        name: "Roast",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/5aday_sweet_potato.jpg/220px-5aday_sweet_potato.jpg",
+        scores: [
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    }
+];
 
 module.exports = friends;
